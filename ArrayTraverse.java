4f8d8b580f43bf733ff64a1f0b1aca906cf16253
Vishal Anil Malapure <155371455+vishalmalapure@users.noreply.github.com>
2025-08-31 23:03:27 +0530

    Add files via upload

---
+++ ArrayTraverse.java
@@ -0,0 +1,15 @@
+class ArrayTraverse
+{
+public static void main(String args[])
+{
+int [] a={10,20};
+for(int ii:a)
+//for(int i=0;i<a.length;i++)
+{
+//System.out.println(a[i]+"");using For loop
+System.out.println(ii+ " ");//Using For each loop
+}
+System.out.println();
+}
+}
+
