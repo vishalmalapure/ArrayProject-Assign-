7ed7496eea979e13821b5aa8a472666fd82bae91
Vishal Anil Malapure <155371455+vishalmalapure@users.noreply.github.com>
2025-09-02 23:28:55 +0530

    Add files via upload

---
+++ KthSmallestElement.java
@@ -0,0 +1,52 @@
+import java.util.*;
+class KthSmallestElement
+{
+   public static void main(String args[])
+   {
+     Scanner sc=new Scanner(System.in);
+     int a[]=new int[10];
+     int i,k,j,count=1,temp,flag=0;
+     System.out.println("Enter 10 elements");
+     for(i=0;i<10;i++)
+      {
+          a[i]=sc.nextInt();
+      }
+     System.out.println("Enter value of k");
+     k=sc.nextInt();
+System.out.prinltn("This is the sorted array");
+
+for(int i=0;i<a.length;i++)
+{
+System.out.prinltn(a[i]);
+}
+     for(i=0;i<10;i++)
+      {
+          for(j=i;j<10;j++)
+           {
+               if(a[i]>a[j])
+                {
+                     temp=a[i];
+                      a[i]=a[j];
+                     a[j]=temp;
+                }
+            }
+        }
+    for(i=0;i<a.length-1;i++)
+        {
+          if(count==k)
+              {
+                     System.out.println(a[i]);
+                     flag=1;
+                     break;
+                }
+          else if(a[i]!=a[i+1])
+               {
+                  count++;
+                }
+           }    
+     if(flag==0)
+             System.out.println("There is not elements");
+
+      
+   }
+}
